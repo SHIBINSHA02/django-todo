711cc466e52ba0ac72ae2e09415ed38c9d2af7d4
SHIBINSHA <shibin24888@gmail.com>
2024-07-25 20:17:53 +0530

    functional approch inview and editing in urls

--- todos/views.py
+++ todos/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views import generic
 from .models import Todo
-from django.http import HttpResponseRedirect
 
-class IndexView(generic.ListView):
-    template_name = 'todos/index.html'
-    context_object_name = 'todo_list'
-
-    def get_queryset(self):
-        """Return all the latest todos."""
-        return Todo.objects.order_by('-created_at')
+def index(request):
+    # Get all todos ordered by creation date in descending order
+    todo_list = Todo.objects.order_by('-created_at')
+    
+    # Render the 'index.html' template with the list of todos
+    return render(request, 'todos/index.html', {'todo_list': todo_list})
 
 def add(request):
-    title = request.POST['title']
-    Todo.objects.create(title=title)
-
-    return redirect('todos:index')
+    if request.method == 'POST':
+        title = request.POST['title']
+        Todo.objects.create(title=title)
+        return redirect('todos:index')
+    else:
+        return render(request, 'todos/add.html')
 
 def delete(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
     todo.delete()
-
     return redirect('todos:index')
 
 def update(request, todo_id):
@@ -28,8 +26,6 @@ def update(request, todo_id):
     isCompleted = request.POST.get('isCompleted', False)
     if isCompleted == 'on':
         isCompleted = True
-    
     todo.isCompleted = isCompleted
-
     todo.save()
     return redirect('todos:index')
